6c754106d24cc81f20b839cc1c97c3e32736d047
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-08-05 22:41:44 -0300

    comentarios

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let num = [5,8,2,9,3]
-num.push(1)
-num.short()
+num.push(0)//inlui numero
+num.sort( )//coloca em ordem
 console.log(num)
-console.log(`o vetor tem ${num.length} posições` )
+console.log(`o vetor tem ${num.length} posições` )//quantidade de array
 console.log(`o primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(4)
+let pos = num.indexOf(5)//qual posição do array
 
 if (pos == -1){
     console.log(`o valor não foi encontrado`)
