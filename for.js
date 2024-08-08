6cc2e84ae436f8d3281f170f7cc143c529db2faf
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-08-07 22:57:26 -0300

    sort

--- for.js
+++ for.js
@@ -1,5 +1,7 @@
 let valores = [1, 4, 6 ,9, 3]
+valores.sort(
 
+)
 console.log(valores)
 
 /*for(let pos = 0; pos <valores.length; pos++){
@@ -7,5 +9,5 @@ console.log(valores)
 }
    */
 for (let pos in valores){
-     console.log(`a posição ${pos} tem o valor` )
+     console.log(`a posição ${pos} tem o valor ${valores[pos]}` )
 }
