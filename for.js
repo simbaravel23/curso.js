37ccfe58408952fd60a6a562bedaf162e5ab2d3d
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-08-07 22:52:10 -0300

    for in

--- for.js
+++ for.js
@@ -2,7 +2,10 @@ let valores = [1, 4, 6 ,9, 3]
 
 console.log(valores)
 
-for(let pos = 0; pos <valores.length; pos++){
+/*for(let pos = 0; pos <valores.length; pos++){
  console.log(`a posição ${pos} tem o valor` )
 }
-   
+   */
+for (let pos in valores){
+     console.log(`a posição ${pos} tem o valor` )
+}
